fa6d403bb11ff4dca067af5b046f29d833e469a5
lipenog <106320475+lipenog@users.noreply.github.com>
2022-08-21 22:33:17 -0300

    Delete trabalho directory

--- trabalho/javascript/script.js
+++
@@ -1,9 +0,0 @@
-import {Cliente} from './Cliente';
-import {Banco} from "./Banco";
-
-const pessoa = new Cliente("ygão", "tonelada")
-const banco = new Banco()
-
-banco.adicionarCliente(pessoa)
-
-console.log(banco.clientesNome("ygão"))
